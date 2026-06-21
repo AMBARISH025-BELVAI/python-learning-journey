c0c5ccd2809d38c6851296dd26405e7947ca1250
AMBARISH BELVAI <ambarishbelvai12@gmail.com>
2026-06-21 19:23:14 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,27 @@
+def greet():
+    print("Welcome")
+
+greet()
+greet()
+#output:#welcome
+        #welcome 
+
+
+
+def multiply(a, b):
+    print(a * b)
+
+multiply(5, 4)
+#output 20
+
+
+def square(num):
+    return num * num
+
+result = square(5)
+
+print(result)
+#output : 25
+
+
+
