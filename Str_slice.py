4ab4f6c39503ea108ae221a411d250f2fd4bd881
AMBARISH BELVAI <ambarishbelvai12@gmail.com>
2026-06-22 07:45:46 +0530

    String slicing in python

---
+++ Str_slice.py
@@ -0,0 +1,20 @@
+name = "Python"
+
+print(name[0:3])
+
+#output : Pyt
+
+#Index:  0 1 2 3 4 5
+#Value:  P y t h o n
+
+name = "Python"
+
+print(name[1:4])
+#output:yth
+
+
+name = "Ambarish"
+
+print(name[0:4])
+
+#output:Amba
