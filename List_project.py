68e088af35cc36b9d3e02699288cfdb491b41aa6
AMBARISH BELVAI <ambarishbelvai12@gmail.com>
2026-06-22 07:51:38 +0530

    Simple List project
    Calculate total of marks and count names in a list.

---
+++ List_project.py
@@ -0,0 +1,19 @@
+marks = [10, 20, 30]
+
+total = 0
+
+for i in marks:
+    total = total + i
+
+print(total)
+#steps:
+#0 + 10 = 10
+#10 + 20 = 30
+#30 + 30 = 60'''
+
+#output:60
+
+names = ["Ambarish", "Ravi", "Kiran"]
+
+print(len(names))
+#output:3
